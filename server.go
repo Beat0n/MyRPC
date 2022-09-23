b6d903a19f48f50dbe32d7045b7f3d54ce57f06b
Beat0n <hujizheuestc@163.com>
2022-09-23 13:47:23 +0800

    ""

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ import (
 	"reflect"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
 
 const MagicNumber = 0x000001
@@ -133,7 +134,7 @@ func (server *Server) sendResponse(cc codec.Codec, header *codec.Header, body in
 	return nil
 }
 
-func (server *Server) findService(serviceMethod string) (*service, *MethodType, error) {
+func (server *Server) findService(serviceMethod string) (*service, *methodType, error) {
 	dot := strings.LastIndex(serviceMethod, ".")
 	if dot <= 0 {
 		err := errors.New("rpc server receive malformed service method")
@@ -163,22 +164,27 @@ type request struct {
 	header       *codec.Header
 	argv, replyv reflect.Value
 	svc          *service
-	methods      *MethodType
+	methods      *methodType
 }
 
-type MethodType struct {
+type methodType struct {
 	//方法
 	method reflect.Method
 	//方法调用需要的参数类型
 	ArgType reflect.Type
 	//方法调用的结果参数类型
 	ReplyType reflect.Type
+	numCalls  uint64
 }
 
-func (mType *MethodType) newArgv() reflect.Value {
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+func (mType *methodType) newArgv() reflect.Value {
 	var argv reflect.Value
 	//// arg may be a pointer type, or a value type
-	if mType.ArgType.Kind() == reflect.Pointer {
+	if mType.ArgType.Kind() == reflect.Ptr {
 		argv = reflect.New(mType.ArgType.Elem())
 	} else {
 		argv = reflect.New(mType.ArgType).Elem()
@@ -186,7 +192,7 @@ func (mType *MethodType) newArgv() reflect.Value {
 	return argv
 }
 
-func (mType *MethodType) newReplyv() reflect.Value {
+func (mType *methodType) newReplyv() reflect.Value {
 	var replyv reflect.Value
 	//reply must be a pointer type
 	switch mType.ReplyType.Elem().Kind() {
@@ -206,7 +212,7 @@ type service struct {
 	//结构体实例
 	rcvr reflect.Value
 	//结构体方法
-	methods map[string]*MethodType
+	methods map[string]*methodType
 }
 
 func newService(rcvr interface{}) *service {
@@ -216,27 +222,26 @@ func newService(rcvr interface{}) *service {
 	s.name = reflect.Indirect(s.rcvr).Type().Name()
 	s.typ = reflect.TypeOf(rcvr)
 	if !ast.IsExported(s.name) {
-		log.Println("rpc server: invalid service")
-		return nil
+		log.Fatalf("rpc server: invalid service %s", s.name)
 	}
 	s.registerMethods()
 	return s
 }
 
 func (s *service) registerMethods() {
-	s.methods = make(map[string]*MethodType)
+	s.methods = make(map[string]*methodType)
 	for i := 0; i < s.typ.NumMethod(); i++ {
 		method := s.typ.Method(i)
 		mType := method.Type
 		//符合条件的方法：3个入参（self， arg， reply），1个出参（error），arg和reply可用
-		if mType.NumIn() != 3 && mType.NumOut() != 1 && mType.Out(0) != reflect.TypeOf(error(nil)) {
+		if mType.NumIn() != 3 || mType.NumOut() != 1 || mType.Out(0) != reflect.TypeOf(error(nil)) {
 			continue
 		}
 		if !isExportedOrBuiltinType(mType.In(1)) || !isExportedOrBuiltinType(mType.In(2)) {
 			continue
 		}
 		argType, replyType := mType.In(1), mType.In(2)
-		s.methods[method.Name] = &MethodType{
+		s.methods[method.Name] = &methodType{
 			method:    method,
 			ArgType:   argType,
 			ReplyType: replyType,
@@ -249,7 +254,9 @@ func isExportedOrBuiltinType(t reflect.Type) bool {
 	return ast.IsExported(t.Name()) || t.PkgPath() == ""
 }
 
-func (s *service) call(m *MethodType, argv, replyv reflect.Value) error {
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+
 	f := m.method.Func
 	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
 	if err := returnValues[0].Interface(); err != nil {
